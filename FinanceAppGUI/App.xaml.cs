a158e65d72485cc311f8bd426e10876b0b58315b
szabopeter1 <peterszabo800@gmail.com>
2024-11-20 16:28:00 +0100

    Add project files.

---
+++ FinanceAppGUI/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace FinanceAppGUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
